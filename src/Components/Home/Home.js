fbf26fecd8e6dcd42b1ea16f4a56557c2a6a6cf5
JANGHYUNn <kimjh2369@gmail.com>
2020-03-02 23:34:11 +0900

    Component real commit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,7 +14,7 @@ class Home extends Component{
 
     const typing = setInterval(() =>{
       const typed = this.state.text;
-      const finalText= "Developer 이규한의 포트폴리오입니다";
+      const finalText= "Developer 김장현의 포트폴리오입니다";
 
       if(i<finalText.length) { 
         this.setState ({
