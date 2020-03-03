7f5a47b567bf4ec31ded02dd69b6dc2d9069e3f3
JANGHYUNn <kimjh2369@gmail.com>
2020-03-03 23:03:07 +0900

    Updates

---
+++ precache-manifest.dd56745f219cb66e5627d6dcb975ffd3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf7cc574da4daf28c4333bbe71f8d9e",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "6eb5cd17b7eb0e965df1",
+    "url": "/react-portfolio/static/css/main.9989870d.chunk.css"
+  },
+  {
+    "revision": "9a90282b3cdffa100687",
+    "url": "/react-portfolio/static/js/2.e598ea76.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-portfolio/static/js/2.e598ea76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eb5cd17b7eb0e965df1",
+    "url": "/react-portfolio/static/js/main.04ca9953.chunk.js"
+  },
+  {
+    "revision": "65015130231404481b6d",
+    "url": "/react-portfolio/static/js/runtime-main.4191cdb0.js"
+  },
+  {
+    "revision": "e7044b300e86588797ad24a3423d30b8",
+    "url": "/react-portfolio/static/media/1.e7044b30.jpg"
+  },
+  {
+    "revision": "11e8af26618daecab1b13cab7387ce65",
+    "url": "/react-portfolio/static/media/900px-JavaScript-logo.11e8af26.png"
+  },
+  {
+    "revision": "faae11a6b87547339411b40ac228d4d7",
+    "url": "/react-portfolio/static/media/css.faae11a6.png"
+  },
+  {
+    "revision": "8c54ec0727b7919b84cd52a2efc47814",
+    "url": "/react-portfolio/static/media/github-logo.8c54ec07.png"
+  },
+  {
+    "revision": "d679dbea0c11e9dfba8dadf6bcac0f7d",
+    "url": "/react-portfolio/static/media/github-logo.d679dbea.svg"
+  },
+  {
+    "revision": "54d7bddcd3019ec3c3b23db00160ccef",
+    "url": "/react-portfolio/static/media/microsoft-sql-server.54d7bddc.svg"
+  },
+  {
+    "revision": "e79fb473204dbd44284bdfb362464254",
+    "url": "/react-portfolio/static/media/react.e79fb473.svg"
+  },
+  {
+    "revision": "b213ea0bb0a64fffd632c82aa3f6cc25",
+    "url": "/react-portfolio/static/media/spring.b213ea0b.png"
+  },
+  {
+    "revision": "46bce9c636ebcc950c845f3442288f37",
+    "url": "/react-portfolio/static/media/subversion-logo.46bce9c6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.8335f0bea94decc7b69142f9b15aa956.js"
+  "/react-portfolio/precache-manifest.dd56745f219cb66e5627d6dcb975ffd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
