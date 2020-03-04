92dfcb7cb7d140089515e82ab243d628a55138af
JANGHYUNn <kimjh2369@gmail.com>
2020-03-04 17:15:13 +0900

    animejs add

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -4,7 +4,7 @@ import Github from '../Project/Row/image/github-logo.svg';
 
 const Contact = () => (
   <div className="Contact" id="c">
-  <h1 className="Contact_hedding">CONTACT!</h1>
+  <h1 className="Contact_hedding">CONTACT</h1>
   <p className="Email"> Email : kimjh2369@gmail.com </p>
  {/* <a href="https://github.com/weaklion?tab=repositories"> <img src={Github} alt="github" className="Github"></img></a> */}
   </div>
