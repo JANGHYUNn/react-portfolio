8d481b14b6851ce8936a2834945765bb18c426c1
JANGHYUNn <kimjh2369@gmail.com>
2020-03-04 21:32:05 +0900

    background modify and animation modify

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,33 +5,45 @@ class Home extends Component{
   constructor(props){
     super(props);
     this.state = {
-      text:""
+      text:"Developer 김장현의 포트폴리오 입니다."
     }
   };
 
   componentDidMount() {
     let i =0;
 
-    const typing = setInterval(() =>{
-      const typed = this.state.text;
-      const finalText= "Developer 김장현의 포트폴리오 입니다.";
+    // const typing = setInterval(() =>{
+    //   const typed = this.state.text;
+    //   const finalText= "Developer 김장현의 포트폴리오 입니다.";
 
-      if(i<finalText.length) { 
-        this.setState ({
-          text: typed + finalText[i]
-        });
-        i++
+    //   if(i<finalText.length) { 
+    //     this.setState ({
+    //       text: typed + finalText[i]
+    //     });
+    //     i++
 
-      } else {
-        clearInterval(typing);
-      }
-    },178);
+    //   } else {
+    //     clearInterval(typing);
+    //   }
+    // },178);
   }
 
   render(){
     return (
   <div className="typer" id="h">
-   <h1 className="typer-text">{this.state.text}</h1>
+    <section className="con">
+    <h1 className="title1">
+      <span>안녕하세요!</span>
+      <span>클릭 해주셔서</span>
+      <span>감사합니다!</span>
+    </h1>
+    <h2 className="title2">
+      <span>Developer</span>
+      <span>김장현의</span>
+      <span>포트폴리오 입니다.</span>
+    </h2>
+  </section>
+   {/* <h1 className="typer-text">{this.state.text}</h1> */}
   </div>
 );
 }
