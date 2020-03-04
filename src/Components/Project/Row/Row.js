626dcdc3619c732ede1f96ac16f5e54b13a54360
JANGHYUNn <kimjh2369@gmail.com>
2020-03-04 17:52:15 +0900

    project update

--- src/Components/Project/Row/Row.js
+++ src/Components/Project/Row/Row.js
@@ -1,32 +1,31 @@
 import React from 'react';
-// import sia from './image/1.png';
+import kcar from './image/kcar.png';
+import hanskin from './image/hanskin.png';
+import namyang from './image/namyang.png';
 import github from './image/github-logo.svg';
 import  './Row.css';
-// import virtual from './image/virtual.png';
-// import smartmirror from './image/smartmirror.png';
+
 
 const Row = () => (
   <div className="Row">
     <div className="Box">
       <div className="Box_image">
-      {/* <a href="https://github.com/weaklion/React-spa"> <img src={sia} alt="sia" className="image"></img></a> */}
+      <a href="https://www.kcar.com/index.do"> <img src={kcar} alt="kcar" className="image"></img></a>
         이미지
       </div>
       <div className="Box_info">
-      <h1 className="Box_title">title</h1>
+      <h1 className="Box_title">K car 직영중고차 리뉴얼</h1>
      
-      <h2 className="name">개발기간</h2>
-      <p className="Box_content">설명
-      </p>
+      <h2 className="name">개발기간 : 2018.07 ~ 2018.11</h2>
+      <p className="Box_content">javascript localstorage 를 이용하여 최근 본 차량 조회 및 jQuery ajax 를 통해 찜목록 조회</p>
       <ul className="Box_ul">   
-      {/* <li  className="Box_li">#React</li>  
-       <li className="Box_li">#Redux</li>  
-       <li className="Box_li">#Node.js(Koa)</li>
-       <li className="Box_li">#MongoDB</li>  */}
+      <li  className="Box_li">#javascript</li>  
+       <li className="Box_li">#jQuery</li>  
+       <li className="Box_li">#Spring</li>
+       <li className="Box_li">#Oracle</li> 
       </ul>
       <ul className="Box_ul">
       <li className="Box_li">
-      {/* <a href="https://github.com/weaklion/React-spa" alt="github"> <img src={github} alt="github" className="icon_image"/></a> */}
        </li>
       </ul>
             </div>
@@ -34,43 +33,39 @@ const Row = () => (
 
      <div className="Box">
       <div className="Box_image">
-      이미지
-      {/* <a href="https://github.com/weaklion/StreetTravel"> <img src={virtual} alt="Virtual" className="image"></img></a> */}
+      <a href="https://www.hanskin.com/main"> <img src={hanskin} alt="Virtual" className="image"></img></a>
       </div>
       <div className="Box_info">
-      <h1 className="Box_title">title</h1>
+      <h1 className="Box_title">셀트리온스킨큐어 한스킨 웹사이트 구축</h1>
 
-      <h2 className="name">개발기간</h2>
-      <p className="Box_content">설명
+      <h2 className="name">개발기간 : 2018.12 ~ 2019.03</h2>
+      <p className="Box_content"> ERP와 EAI ( 두 시스템의 데이터 연동 )를 통해 송/수신 프로시저 작성
       </p>
       <ul className="Box_ul">   
-      {/* <li  className="Box_li">#Java</li>  
-       <li className="Box_li">#GoogleAPI</li>  
-       <li className="Box_li">#Android</li> */}
+      <li  className="Box_li">#msSql</li>  
+       <li className="Box_li">#Sql server</li>  
+       <li className="Box_li">#T-SQL</li>
       </ul>
       <ul className="Box_ul">
       <li className="Box_li">
-      {/* <a href="https://github.com/weaklion/StreetTravel" alt="github"> <img src={github} alt="github" className="icon_image"/></a> */}
        </li>
       </ul>
             </div>
     </div>
     <div className="Box">
        <div className="Box_image">
-       이미지
-       {/* <a href="https://github.com/weaklion/Jarvis_Mirror"> <img src={smartmirror} alt="smartMirror" className="image"></img> </a> */}
+       <a href="https://shopping.namyangi.com/main/main"> <img src={namyang} alt="namyang" className="image"></img> </a>
       </div>
       <div className="Box_info">
-      <h1 className="Box_title">title</h1>
+      <h1 className="Box_title">남양유업 커머스 구축</h1>
      
-      <h2 className="name">개발기간</h2>
-      <p className="Box_content">설명
+      <h2 className="name">개발기간 : 2019.04 ~ 2019.09</h2>
+      <p className="Box_content">ERP와 EAI ( 두 시스템의 데이터 연동 )를 통해 송/수신 프로시저 작성
       </p>
       <ul className="Box_ul">   
-      {/* <li  className="Box_li">#JavaScript</li>  
-       <li className="Box_li">#Raspberry Pi</li>  
-       <li className="Box_li">#Node.js</li>
-       <li  className="Box_li">#Electron</li>  */}
+      <li  className="Box_li">#msSql</li>  
+       <li className="Box_li">#Sql server</li>  
+       <li className="Box_li">#T-SQL</li>
       </ul>
       <ul className="Box_ul">
       <li className="Box_li">
@@ -79,6 +74,27 @@ const Row = () => (
       </ul>
             </div>
             </div>
+            <div className="Box">
+      <div className="Box_image">
+      {/* <a href="https://www.hanskin.com/main"> <img src={hanskin} alt="Virtual" className="image"></img></a> */}
+      </div>
+      <div className="Box_info">
+      <h1 className="Box_title">포스코 차세대 생산, 물류관리 시스템</h1>
+
+      <h2 className="name">개발기간 : 2019.11 ~ 2020.01</h2>
+      <p className="Box_content"> 작업지시서 등록 및 물류관리 조회 기능 개발
+      </p>
+      <ul className="Box_ul">   
+      <li  className="Box_li">#react</li>  
+       <li className="Box_li">#mobx</li>  
+       <li className="Box_li">#axios</li>
+      </ul>
+      <ul className="Box_ul">
+      <li className="Box_li">
+       </li>
+      </ul>
+            </div>
+    </div>
     </div>
 );
 
