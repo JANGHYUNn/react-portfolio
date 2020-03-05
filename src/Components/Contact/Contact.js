a8a196af33c0b40360a663634cad27a882563164
JANGHYUNn <kimjh2369@gmail.com>
2020-03-05 10:49:31 +0900

    About me update

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => (
   <div className="Contact" id="c">
   <h1 className="Contact_hedding">CONTACT</h1>
   <p className="Email"> Email : kimjh2369@gmail.com </p>
- {/* <a href="https://github.com/weaklion?tab=repositories"> <img src={Github} alt="github" className="Github"></img></a> */}
+ <a href="https://github.com/JANGHYUNn?tab=repositories"> <img src={Github} alt="github" className="Github"></img></a>
   </div>
 );
 
