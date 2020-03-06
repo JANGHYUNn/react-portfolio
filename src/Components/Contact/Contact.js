565404e133b6514c5b21eb81048c704ef71f826f
JANGHYUNn <kimjh2369@gmail.com>
2020-03-06 10:06:05 +0900

    project modify

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -5,7 +5,9 @@ import Github from '../Project/Row/image/github-logo.svg';
 const Contact = () => (
   <div className="Contact" id="c">
   <h1 className="Contact_hedding">CONTACT</h1>
-  <p className="Email"> Email : kimjh2369@gmail.com </p>
+  <p className="Email"> 김장현 </p>
+  <p className="Email"> kimjh2369@gmail.com </p>
+  <p className="Email"> 010-2418-2369 </p>
  <a href="https://github.com/JANGHYUNn?tab=repositories"> <img src={Github} alt="github" className="Github"></img></a>
   </div>
 );
--- src/Components/Project/Row/Row.js
+++ src/Components/Project/Row/Row.js
@@ -16,7 +16,12 @@ const Row = () => (
       <h1 className="Box_title">K car 직영중고차 리뉴얼</h1>
      
       <h2 className="name">개발기간 : 2018.07 ~ 2018.11</h2>
-      <p className="Box_content">javascript localstorage 를 이용하여 최근 본 차량 조회 및 jQuery ajax 를 통해 찜목록 조회</p>
+      <p className="Box_content">
+            - 화면 기획서를 검토 후 기획자와의 소통 후 개발 진행<br/>
+            - jQuery를 이용하여 각 이벤트를 주었지만 노드생성 시점 차이로 문제발생<br/>
+            - $(document).ready 와 window.onload 에 차이점을 이해하고 문제해결<br/>
+            - javascript localstorage 를 이용하여 최근 본 차량 조회 및 jQuery ajax 를 통해 찜목록 조회
+      </p>
       <ul className="Box_ul">   
       <li  className="Box_li">#javascript</li>  
        <li className="Box_li">#jQuery</li>  
@@ -38,7 +43,11 @@ const Row = () => (
       <h1 className="Box_title">셀트리온스킨큐어 한스킨 웹사이트 구축</h1>
 
       <h2 className="name">개발기간 : 2018.12 ~ 2019.03</h2>
-      <p className="Box_content"> ERP와 EAI ( 두 시스템의 데이터 연동 )를 통해 송/수신 프로시저 작성
+      <p className="Box_content">
+            - 처음해보는 프로시저 개발이어서 지식이 부족한 문제발생<br/>
+            - 각종 문서와 선임에게 질문을 통해 문제해결<br/>
+            - 인터페이스 설계서를 본 후 Project leader와 데이터 format 및 타입 정의<br/>
+            - ERP와 EAI ( 두 시스템의 데이터 연동 )를 통해 송/수신 프로시저 작성
       </p>
       <ul className="Box_ul">   
       <li  className="Box_li">#msSql</li>  
@@ -59,7 +68,11 @@ const Row = () => (
       <h1 className="Box_title">남양유업 커머스 구축</h1>
      
       <h2 className="name">개발기간 : 2019.04 ~ 2019.09</h2>
-      <p className="Box_content">ERP와 EAI ( 두 시스템의 데이터 연동 )를 통해 송/수신 프로시저 작성
+      <p className="Box_content">
+            - 이전 경험을 통해 원활히 진행<br/>
+            - 선임에 개발건중 cross domain 개발건이 있어 도움이 되고싶어 관련 지식을 찾다가 CORS정책에 대해서 이해<br/>
+            - 서버쪽에 response.header 설정을 통해 해결<br/>
+            - ERP와 EAI ( 두 시스템의 데이터 연동 )를 통해 송/수신 프로시저 작성
       </p>
       <ul className="Box_ul">   
       <li  className="Box_li">#msSql</li>  
@@ -81,7 +94,11 @@ const Row = () => (
       <h1 className="Box_title">포스코 차세대 생산, 물류관리 시스템</h1>
 
       <h2 className="name">개발기간 : 2019.11 ~ 2020.01</h2>
-      <p className="Box_content"> 작업지시서 등록 및 물류관리 조회 기능 개발
+      <p className="Box_content">
+            - es6 문법에 대해 지식이 부족하다 느낌<br/>
+            - 스터디를 통해 es6 문법과 실행 컨텍스트 이해 <br/>
+            - 백엔드 개발자와 협의 후 axios 를 이용해 http통신하여 데이터 가공 후 물류관리 조회, 작업지시서 등록<br/>
+            - mobx를 통한 state 상태관리
       </p>
       <ul className="Box_ul">   
       <li  className="Box_li">#react</li>  
